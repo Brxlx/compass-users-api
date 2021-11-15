78fba06480d61f27757fae17ce706831bc843fc5
Bruno Santos <brcesar19@gmail.com>
2021-11-15 12:13:20 -0300

    chore: dynamic prefix with env

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import 'dotenv/config';
 
 // import x from './states.routes';
-import { userRouter } from './users/users.routes';
+import { userRouter } from './users.routes';
 
 const routes = Router();
 
-routes.use('/v1', userRouter);
+routes.use(`/${process.env.APP_PREFIX}`, userRouter);
 
 export { routes };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import { app } from './app';
 
 const APP_PORT = Number(process.env.APP_PORT) || 3000;
+
 app.listen(APP_PORT, () => {
   console.log(`App running on port ${APP_PORT}`);
 });
