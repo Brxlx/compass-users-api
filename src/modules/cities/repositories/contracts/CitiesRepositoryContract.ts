13e40bda068cd4f9335eecfa9aba71990cb40313
Bruno Santos <brcesar19@gmail.com>
2021-11-18 10:47:40 -0300

    chore: dto on create city

--- src/modules/cities/repositories/contracts/CitiesRepositoryContract.ts
+++ src/modules/cities/repositories/contracts/CitiesRepositoryContract.ts
@@ -1,12 +1,12 @@
-// import {} from '../../dtos';
-
 import { City } from '.prisma/client';
+
+import { ICreateCityDTO } from '../../dtos/ICreateCityDTO';
 
 abstract class CitiesRepsitoryContract {
   public getCityByName: (name: string) => Promise<City>;
 
   public getCityByState: (state: string) => Promise<City[]>;
 
-  public createCity: ({ name, state }: Omit<City, 'id'>) => Promise<City>;
+  public createCity: ({ name, state }: ICreateCityDTO) => Promise<City>;
 }
 export { CitiesRepsitoryContract };
